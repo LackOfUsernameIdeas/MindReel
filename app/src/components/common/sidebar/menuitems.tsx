7d2b06e3ef7c5da642d2d31eca2b8b867d9e1846
LackOfUsernameIdeas <kaloyan.kostadinov2007@abv.bg>
2025-07-28 16:02:54 +0300

    Update menuitems.tsx

--- app/src/components/common/sidebar/menuitems.tsx
+++ app/src/components/common/sidebar/menuitems.tsx
@@ -138,32 +138,6 @@ export const MENUITEMS = [
       }
     ]
   },
-  // {
-  //   icon: <i className="side-menu__icon bx bx-bar-chart-alt-2"></i>,
-  //   type: "sub",
-  //   Name: "",
-  //   active: false,
-  //   selected: false,
-  //   title: "Индивидуални Статистики",
-  //   class:
-  //     "badge !bg-warning/10 !text-warning !py-[0.25rem] !px-[0.45rem] !text-[0.75em] ms-2",
-  //   children: [
-  //     {
-  //       path: `${import.meta.env.BASE_URL}app/individualStats/movies_series`,
-  //       type: "link",
-  //       active: false,
-  //       selected: false,
-  //       title: "За Филми/Сериали"
-  //     },
-  //     {
-  //       path: `${import.meta.env.BASE_URL}app/individualStats/books`,
-  //       type: "link",
-  //       active: false,
-  //       selected: false,
-  //       title: "За Книги"
-  //     }
-  //   ]
-  // },
   {
     path: `${import.meta.env.BASE_URL}app/individualStats/movies_series`,
     icon: <i className="side-menu__icon bx bx-bar-chart-alt-2"></i>,
@@ -175,39 +149,6 @@ export const MENUITEMS = [
     class:
       "badge !bg-warning/10 !text-warning !py-[0.25rem] !px-[0.45rem] !text-[0.75em] ms-2"
   },
-  // {
-  //   icon: <i className="side-menu__icon bx bx-bar-chart-alt-2"></i>,
-  //   type: "sub",
-  //   Name: "",
-  //   active: false,
-  //   selected: false,
-  //   title: "Общи статистики",
-  //   class:
-  //     "badge !bg-warning/10 !text-warning !py-[0.25rem] !px-[0.45rem] !text-[0.75em] ms-2",
-  //   children: [
-  //     {
-  //       path: ``,
-  //       type: "link",
-  //       active: false,
-  //       selected: false,
-  //       title: "Жанрове"
-  //     },
-  //     {
-  //       path: ``,
-  //       type: "link",
-  //       active: false,
-  //       selected: false,
-  //       title: "Просперитет"
-  //     },
-  //     {
-  //       path: ``,
-  //       type: "link",
-  //       active: false,
-  //       selected: false,
-  //       title: "Актьори"
-  //     }
-  //   ]
-  // },
   {
     menutitle: "КОНТАКТ"
   },
